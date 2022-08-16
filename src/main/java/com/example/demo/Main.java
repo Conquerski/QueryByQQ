9279209b465a4315249e7784fb57f24bc2fd38ee
ConquerSki <yifanski@gmail.com>
2022-08-16 20:49:25 +0800

    啊哈

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import me.dreamvoid.miraimc.api.MiraiBot;
-import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiFriendMessageEvent;
 import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -20,15 +19,11 @@ public class Main extends JavaPlugin implements Listener {
     @Override // 禁用插件
     public void onDisable() { }
 
-    @EventHandler
-    public void onFriendMessageReceive(MiraiFriendMessageEvent e){
-        getLogger().info("接收到好友"+e.getSenderID()+"的消息: "+e.getMessage());
-        MiraiBot.getBot(e.getBotID()).getFriend(e.getSenderID()).sendMessage("你发送了一条消息："+e.getMessage());
-    }
 
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
-        getLogger().info("接收到群聊"+e.getGroupID()+"的消息: "+e.getMessage());
-        MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessageMirai("[mirai:at:"+e.getSenderID()+"] 你发送了一条消息："+e.getMessage());
+        if(e.getMessage().equals("在线人数")) {
+            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("当前在线人数：" + Bukkit.getServer().getOnlinePlayers().size()+"人");
+        }
     }
 }
