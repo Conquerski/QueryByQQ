29e4919d1a07c6a85c03888cda3ad5300b7697a7
ConquerSki <yifanski@gmail.com>
2022-08-16 20:55:44 +0800

    更新List

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends JavaPlugin implements Listener {
     @Override // 加载插件
@@ -22,8 +26,16 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
-        if(e.getMessage().equals("在线人数")) {
-            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("当前在线人数：" + Bukkit.getServer().getOnlinePlayers().size()+"人");
+        if(e.getMessage().equals("在线玩家")) {
+            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("当前在线玩家：" + getPlayerNameListOnline() +"人");
         }
+    }
+
+    public static List<String> getPlayerNameListOnline() {
+        List<String> l = new ArrayList<>();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            l.add(player.getDisplayName());
+        }
+        return l;
     }
 }
